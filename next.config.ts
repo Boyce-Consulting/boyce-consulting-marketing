647045d4a2edf139f324356f7a3ee27c887c1271
Boyce-Consulting <165805989+Boyce-Consulting@users.noreply.github.com>
2025-11-11 17:24:08 -0700

    ddd

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/boyce-consulting-marketing',
   images: {
     unoptimized: true,
   },
